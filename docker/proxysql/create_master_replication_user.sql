e1fee49394a6415652b4c440f8dfe3097c813b62
jorge <jorge@jorge.jorge>
2026-02-28 12:50:15 -0300

    feat(scripts) scripts to run proxysql, master and repl

--- docker/proxysql/create_master_replication_user.sql
+++
@@ -1,9 +0,0 @@
-CREATE USER 'repl_user'@'%' 
-IDENTIFIED WITH caching_sha2_password 
-BY 'secret123';
-
-GRANT REPLICATION SLAVE, REPLICATION CLIENT
-ON *.*
-TO 'repl_user'@'%';
-
-FLUSH PRIVILEGES;
--- docker/proxysql/nodes_default_user.sql
+++
@@ -1,4 +0,0 @@
-CREATE USER 'proxysql_user'@'%' IDENTIFIED BY "secret";
-
-GRANT ALL PRIVILEGES ON *.* TO 'proxysql_user'@'%';
-FLUSH PRIVILEGES;
